208f5b1efe37b11453563a9d1984d71ab6ff94fc
Peter Richards <peter.richards@thefuturetoday.net>
2020-09-24 17:50:14 +0100

    Added local server port to response

--- src/main/java/net/thefuturetoday/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ src/main/java/net/thefuturetoday/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,5 +1,7 @@
 package net.thefuturetoday.microservices.currencyexchangeservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +10,17 @@ import java.math.BigDecimal;
 
 @RestController
 public class CurrencyExchangeController {
+    private final Environment environment;
+
+    public CurrencyExchangeController(Environment environment) {
+        this.environment = environment;
+    }
+
     @GetMapping("/currency-exchange/from/{from}/to/{to}")       //where {from} and {to} are path variable
     public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to)  //from map to USD and to map to INR
     {
-        return new  ExchangeValue(1000L, from, to, BigDecimal.valueOf(65));
+        ExchangeValue exchangeValue = new  ExchangeValue(1000L, from, to, BigDecimal.valueOf(65));
+        exchangeValue.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
+        return exchangeValue;
     }
 }
