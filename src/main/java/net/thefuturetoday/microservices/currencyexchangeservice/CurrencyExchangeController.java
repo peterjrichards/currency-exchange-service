c7ae6adc139d743d0a29f871e44162f3e781b06f
Peter Richards <peter.richards@thefuturetoday.net>
2020-09-24 20:43:03 +0100

    Added Spring Sleuth

--- src/main/java/net/thefuturetoday/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ src/main/java/net/thefuturetoday/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,5 +1,7 @@
 package net.thefuturetoday.microservices.currencyexchangeservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +12,8 @@ import java.math.BigDecimal;
 
 @RestController
 public class CurrencyExchangeController {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
     private final Environment environment;
     private final ExchangeValueRepository repository;
 
@@ -23,6 +27,7 @@ public class CurrencyExchangeController {
     {
         ExchangeValue exchangeValue=repository.findByFromAndTo(from,to);
         exchangeValue.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
+        logger.info("{}", exchangeValue);
         return exchangeValue;
     }
 }
--- src/main/java/net/thefuturetoday/microservices/currencyexchangeservice/CurrencyExchangeServiceApplication.java
+++ src/main/java/net/thefuturetoday/microservices/currencyexchangeservice/CurrencyExchangeServiceApplication.java
@@ -1,8 +1,10 @@
 package net.thefuturetoday.microservices.currencyexchangeservice;
 
+import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -12,4 +14,10 @@ public class CurrencyExchangeServiceApplication {
 		SpringApplication.run(CurrencyExchangeServiceApplication.class, args);
 	}
 
+	@Bean
+	//creating a sampler called always sampler
+	public Sampler defaultSampler()
+	{
+		return Sampler.ALWAYS_SAMPLE;
+	}
 }
